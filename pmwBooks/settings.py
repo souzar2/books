7ce92b31f2559ad67f8e474417c710483d6d27ce
souzar2 <arthursouzacomp@gmail.com>
2023-10-10 16:20:57 -0300

    Projeto finalizado

--- pmwBooks/settings.py
+++ pmwBooks/settings.py
@@ -146,8 +146,14 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #E-mail
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_LTS', default=True)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST = config('EMAIL_HOST')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 LOGIN_REDIRECT_URL = 'books'
